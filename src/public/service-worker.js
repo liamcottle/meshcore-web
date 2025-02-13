e38033d0247c9aeea5b2cc47216b527d2137f47f
liamcottle <liam@liamcottle.com>
2025-02-13 18:52:36 +1300

    dismiss notification on click

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -2,3 +2,11 @@ self.addEventListener('fetch',function() {
     // this is required to meet the requirements for an installable pwa
     // it allows the browser to ask the user if they want to install to their homescreen
 });
+
+// handle push notification click
+self.addEventListener('notificationclick', function(event) {
+
+    // dismiss it
+    event.notification.close();
+
+});
