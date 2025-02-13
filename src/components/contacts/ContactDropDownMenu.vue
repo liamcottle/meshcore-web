665ffd0a7600ed041245492c7ad307ae50318020
liamcottle <liam@liamcottle.com>
2025-02-13 17:20:08 +1300

    add button to copy public key to clipboard

--- src/components/contacts/ContactDropDownMenu.vue
+++ src/components/contacts/ContactDropDownMenu.vue
@@ -25,6 +25,14 @@
                 </div>
             </div>
 
+            <!-- copy public key button -->
+            <DropDownMenuItem @click="copyPublicKey(contact)">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
+                    <path fill-rule="evenodd" d="M15.75 1.5a6.75 6.75 0 0 0-6.651 7.906c.067.39-.032.717-.221.906l-6.5 6.499a3 3 0 0 0-.878 2.121v2.818c0 .414.336.75.75.75H6a.75.75 0 0 0 .75-.75v-1.5h1.5A.75.75 0 0 0 9 19.5V18h1.5a.75.75 0 0 0 .53-.22l2.658-2.658c.19-.189.517-.288.906-.22A6.75 6.75 0 1 0 15.75 1.5Zm0 3a.75.75 0 0 0 0 1.5A2.25 2.25 0 0 1 18 8.25a.75.75 0 0 0 1.5 0 3.75 3.75 0 0 0-3.75-3.75Z" clip-rule="evenodd" />
+                </svg>
+                <span>Copy Public Key</span>
+            </DropDownMenuItem>
+
             <!-- reset path button -->
             <DropDownMenuItem @click="onResetPath(contact)">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
@@ -59,6 +67,7 @@ import DropDownMenu from "../DropDownMenu.vue";
 import DropDownMenuItem from "../DropDownMenuItem.vue";
 import Connection from "../../js/Connection.js";
 import Database from "../../js/Database.js";
+import Utils from "../../js/Utils.js";
 
 export default {
     name: 'ContactDropDownMenu',
@@ -75,6 +84,9 @@ export default {
         "contact-deleted",
     ],
     methods: {
+        copyPublicKey(contact) {
+            Utils.copyToClipboard(Utils.bytesToHex(contact.publicKey));
+        },
         async onResetPath(contact) {
 
             // confirm user wants to reset path
--- src/js/Utils.js
+++ src/js/Utils.js
@@ -8,6 +8,22 @@ class Utils {
         return Array.from(uint8Array).map(byte => byte.toString(16).padStart(2, '0')).join('');
     }
 
+    static async copyToClipboard(text) {
+
+        // make sure copy to clipboard is supported
+        if(!navigator.clipboard || !navigator.clipboard.writeText){
+            alert("Clipboard not supported. Site must be served via https on iOS.");
+            return;
+        }
+
+        // copy value to clipboard
+        await navigator.clipboard.writeText(text);
+
+        // tell user we copied it
+        alert("Copied to clipboard!");
+
+    }
+
     static isUint8ArrayEqual(a, b) {
 
         // ensure they are the same length
