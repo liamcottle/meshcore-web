957c4799ead3e8e05fb4b7370779ccc9e92e30a6
liamcottle <liam@liamcottle.com>
2025-02-16 01:54:16 +1300

    remove duplicate class

--- src/components/contacts/ContactIcon.vue
+++ src/components/contacts/ContactIcon.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="flex rounded-full h-12 w-12 text-white shadow bg-[#607e8c] text-white">
+        <div class="flex rounded-full h-12 w-12 text-white shadow bg-[#607e8c]">
             <div class="mx-auto my-auto drop-shadow-sm">
                 <svg v-if="contact.type === Constants.AdvType.Chat" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
                     <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
