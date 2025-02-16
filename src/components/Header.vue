4b69453ee0ba1bc0b98d276d8fdfd8f9b6ff07dd
liamcottle <liam@liamcottle.com>
2025-02-16 17:59:27 +1300

    don't shrink logo when device name too long

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex bg-white p-2 border-b h-16">
-        <div class="my-auto mr-2">
+        <div class="flex-shrink-0 my-auto mr-2">
             <img src="/icon.png" class="size-12 rounded"/>
         </div>
         <div class="my-auto mr-auto overflow-hidden">
